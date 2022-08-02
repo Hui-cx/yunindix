4faa58a255577e868cd6a25e94a794a3d36b4da6
chenxiaohui <chenxiaohui@situdata.com>
2022-08-02 14:56:44 +0800

    创建仓库

---
+++ src/core/commands/terminal/info/InfoBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>关于本站：YuIndex 极客范儿的浏览器主页</div>
+  <div>
+    <a href="javascript:;" target="_blank">
+      代码开源，欢迎 star 和贡献~
+    </a>
+  </div>
+  <div></div>
+  <div>
+    作者：
+    <a href="javascript:;" target="_blank">
+      程序员陈晓辉
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface InfoBoxProps {
+  seconds: string;
+}
+
+const props = withDefaults(defineProps<InfoBoxProps>(), {});
+</script>
+
+<style scoped></style>
